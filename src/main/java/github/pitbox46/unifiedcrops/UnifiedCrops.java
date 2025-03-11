e2e70b7d7ccc0a43b73127d6ab333fd883f6044c
pitbox <migriswo@iu.edu>
2025-03-11 16:47:04 -0400

    Init

---
+++ src/main/java/github/pitbox46/unifiedcrops/UnifiedCrops.java
@@ -0,0 +1,16 @@
+package github.pitbox46.unifiedcrops;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(UnifiedCrops.MODID)
+public class UnifiedCrops {
+    public static final String MODID = "unifiedcrops";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public UnifiedCrops(ModContainer container) {
+
+    }
+}
